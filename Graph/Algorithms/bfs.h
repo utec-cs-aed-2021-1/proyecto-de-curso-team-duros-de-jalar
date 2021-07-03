39d149d65ed78449b39206dd0350139b089dfad5
Marcos-1001 <75715909+Marcos-1001@users.noreply.github.com>
2021-07-02 23:00:31 -0500

    Add files via upload

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -1,7 +1,7 @@
 #include <queue>
 #include <unordered_set>
-#include "..\DirectedGraph.h"
-#include "..\UndirectedGraph.h"
+#include "DirectedGraph.h"
+#include "UndirectedGraph.h"
 using namespace std;
 
 template<typename TV, typename TE>
@@ -11,19 +11,18 @@ private:
 public:
     bfs() = default;
     bfs(Graph<TV,TE>* &grafo, string vertexid) {
-        std::unordered_set<Vertex<TV,TE>*> visited;
+        std::unordered_set<string> visited;
         std::queue<pair<string ,Vertex<TV,TE>*>> cola;
 
         auto aux = grafo->vertexes[vertexid];
         G = new DirectedGraph<TV,TE>;
         G->insertVertex(vertexid, aux->data);
-        visited.insert(aux);
+        visited.insert(vertexid);
 
         for(auto i : aux->edges){
             Vertex<TV,TE>* ax = i->vertexes[1];
-            if(visited.find(ax) ==visited.end()){
+            if(visited.find(ax->id) ==visited.end()){
                 cola.push(make_pair( aux->id, ax));
-                visited.insert(ax);
             }
         }
 
@@ -35,14 +34,17 @@ public:
             string id; Vertex<TV,TE>* to_insert;
             id = res.first;
             to_insert = res.second;
-            G->insertVertex(to_insert->id, to_insert->data);
-            G->createEdge(id, to_insert->id, 1);
+
+            if(visited.find(to_insert->id) ==visited.end()) {
+                visited.insert(to_insert->id);
+                G->insertVertex(to_insert->id, to_insert->data);
+                G->createEdge(id, to_insert->id, 1);
+            }
 
             for(auto i : to_insert->edges){
                 Vertex<TV,TE>* ax = i->vertexes[1];
-                if(visited.find(ax) ==visited.end()){
+            if(visited.find(ax->id) ==visited.end()){
                     cola.push(make_pair(to_insert->id,ax));
-                    visited.insert(ax);
                 }
             }
         }
@@ -51,5 +53,5 @@ public:
     DirectedGraph<TV,TE>* apply(){
         return G;
     }
-    
+
 };
--- Graph/Algorithms/dfs.h
+++ Graph/Algorithms/dfs.h
@@ -1,7 +1,7 @@
 #include <stack>
 #include <unordered_set>
-#include "..\DirectedGraph.h"
-#include "..\UndirectedGraph.h"
+#include "DirectedGraph.h"
+#include "UndirectedGraph.h"
 using namespace std;
 
 
@@ -12,23 +12,21 @@ private:
 public:
     dfs() = default;
     dfs(Graph<TV,TE>* &grafo, string vertexid) {
-        std::unordered_set<Vertex<TV,TE>*> visited;
+        std::unordered_set<string> visited;
         std::stack<pair<string ,Vertex<TV,TE>*>> pila;
 
         auto aux = grafo->vertexes[vertexid];
         G = new DirectedGraph<TV,TE>;
         G->insertVertex(vertexid, aux->data);
 
-        visited.insert(aux); //PREGUNTAR AL PROFE
+        visited.insert(vertexid); //PREGUNTAR AL PROFE
 
-        for(auto i : aux->edges){
+        for(auto& i : aux->edges){
             Vertex<TV,TE>* ax = i->vertexes[1];
-            if(visited.find(ax) ==visited.end()){
+            if(visited.find(ax->id) ==visited.end()){
                 pila.push(make_pair( aux->id, ax));
             }
         }
-
-
 
         while(!pila.empty()){
             pair<string ,Vertex<TV,TE>*> res = pila.top();
@@ -36,13 +34,16 @@ public:
             string id; Vertex<TV,TE>* to_insert;
             id = res.first;
             to_insert = res.second;
-            visited.insert(to_insert);
-            G->insertVertex(to_insert->id, to_insert->data);
-            G->createEdge(id, to_insert->id, 1);
 
-            for(auto i : to_insert->edges){
+            if(visited.find(to_insert->id) ==visited.end()) {
+                visited.insert(to_insert->id);
+                G->insertVertex(to_insert->id, to_insert->data);
+                G->createEdge(id, to_insert->id, 1);
+            }
+
+            for(auto& i : to_insert->edges){
                 Vertex<TV,TE>* ax = i->vertexes[1];
-                if(visited.find(ax) ==visited.end()){
+                if(visited.find(ax->id) ==visited.end()){
                     pila.push(make_pair(to_insert->id,ax));
                 }
             }
