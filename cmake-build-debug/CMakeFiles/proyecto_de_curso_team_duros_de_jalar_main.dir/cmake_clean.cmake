9b85df05913e613b915d57d8bf2e2f9f0e9938aa
Luis-ntonio <63657624+Luis-ntonio@users.noreply.github.com>
2021-07-05 10:23:04 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/proyecto_de_curso_team_duros_de_jalar_main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto_de_curso_team_duros_de_jalar_main.dir/main.cpp.obj"
+  "proyecto_de_curso_team_duros_de_jalar_main.exe"
+  "proyecto_de_curso_team_duros_de_jalar_main.exe.manifest"
+  "proyecto_de_curso_team_duros_de_jalar_main.lib"
+  "proyecto_de_curso_team_duros_de_jalar_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto_de_curso_team_duros_de_jalar_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
