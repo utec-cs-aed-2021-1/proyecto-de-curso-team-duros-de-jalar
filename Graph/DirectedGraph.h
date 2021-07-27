0ea3a9fce032fecb7b2113550229de4b833c3ee1
Marcos-1001 <75715909+Marcos-1001@users.noreply.github.com>
2021-07-27 17:37:35 -0500

    Update DirectedGraph.h

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -73,7 +73,7 @@ bool DirectedGraph<TV, TE>::insertVertex(string id, TV vertex) {
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::createEdge(string id1, string id2, TE w) {
-    if (this->vertexes.find(id1) == this->vertexes.end() && this->vertexes.find(id2) == this->vertexes.end())
+    if (this->vertexes.find(id1) == this->vertexes.end() || this->vertexes.find(id2) == this->vertexes.end())
         return false;
 
     auto *new_edge = new Edge<TV, TE>;
